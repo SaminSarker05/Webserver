0c23166b298f0a640183b14dd56cc0f5054583cd
Samin Sarker <saminsarker05@gmail.com>
2026-04-21 13:02:56 -0400

    simple communication between client and server added

--- client.cpp
+++ client.cpp
@@ -2,6 +2,8 @@
 #include <thread>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <sys/socket.h>
+#include <string.h>
 
 int main() {
         // create a TCP socket accepting IPv4 addresses
@@ -11,9 +13,14 @@ int main() {
         struct sockaddr_in addr;
         addr.sin_family = AF_INET;
         addr.sin_port = htons(8080);
-        inet_pton(AF_INET, "127.0.0.01", &addr.sin_addr);
+        inet_pton(AF_INET, "127.0.0.1", &addr.sin_addr);
 
         connect(socket_fd, (struct sockaddr *) &addr, sizeof(addr));
+
+        std::string buffer;
+        std::cout << "Enter a message to send to the server: ";
+        std::getline(std::cin, buffer);
+        send(socket_fd, buffer.c_str(), buffer.length(), 0);
 
         close(socket_fd);
 }
--- server.cpp
+++ server.cpp
@@ -4,12 +4,20 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
-class Connection {
-        int fd;
-        std::string ip_address;
-        uint16_t port;
-        std::string status;
-};
+void handle_connection(int client_fd) {
+        std::string message;
+        char buffer[1024];  // ~1KB buffer to read data
+        int bytes_read;
+        while ((bytes_read = recv(client_fd, buffer, sizeof(buffer) - 1, 0)) > 0) {
+                buffer[bytes_read] = '\0';
+                message.append(buffer, bytes_read);
+        }
+        if (message[message.length() - 1] == '\n') {
+                message[message.length() - 1] = '\0';  // remove newline char
+        }
+        std::cout << client_fd << " said: " << message << std::endl;
+        close(client_fd);
+}
 
 int main() {
         // create a TCP socket accepting IPv4 addresses
@@ -19,19 +27,23 @@ int main() {
         struct sockaddr_in addr;
         addr.sin_family = AF_INET;
         addr.sin_port = htons(8080);
-        addr.sin_addr.s_addr = INADDR_ANY;  // accept connections from all interfaces
+        addr.sin_addr.s_addr = INADDR_ANY;
 
+        // bind socket to all interfaces on port 8080
         bind(server_fd, (sockaddr*) &addr, sizeof(addr));
         listen(server_fd, SOMAXCONN);
         std::cout << "server listening on port 8080..." << std::endl;
 
         while (true) {
+                // wait for and accept incoming connections
                 int client_fd = accept(server_fd, NULL, NULL);
                 if (client_fd < 0)
                         continue;
-                std::cout << client_fd << std::endl;
+
+                handle_connection(client_fd);
         }
 
         close(server_fd);
+        return 0;
 }
 
